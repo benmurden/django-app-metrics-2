5f8bea32a5534f79be315eabd68296de8f9a66ae
Benjamin Murden <ben.murden@gmail.com>
2017-11-29 21:11:26 +0900

    Update django test versions and fix deprecations.

--- app_metrics/backends/composite.py
+++ app_metrics/backends/composite.py
@@ -1,5 +1,6 @@
+from importlib import import_module
+
 from django.conf import settings
-from django.utils.importlib import import_module
 
 DEFAULT_BACKENDS = getattr(settings, 'APP_METRICS_COMPOSITE_BACKENDS', [])
 
