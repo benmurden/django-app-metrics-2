bc16b2d9e66d965e0fd9383358e51ce8f9cefe7d
Benjamin Murden <ben.murden@gmail.com>
2018-06-07 16:13:54 +0900

    Use `render` instead of `render_to_response`

--- app_metrics/views.py
+++ app_metrics/views.py
@@ -1,11 +1,6 @@
-
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render_to_response
-from django.template import RequestContext
+from django.shortcuts import render
 
 @login_required
 def metric_report_view(request):
-    return render_to_response('app_metrics/reports.html', {}, context_instance=RequestContext(request))
-
-
-
+    return render(request, 'app_metrics/reports.html')
