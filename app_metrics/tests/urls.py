1ca1b503f0f1094d9acd5dba08b27f4915af673e
Benjamin Murden <ben.murden@gmail.com>
2019-08-05 14:18:52 +0900

    Fix included urls for django 2.1

--- app_metrics/tests/urls.py
+++ app_metrics/tests/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
     url(r'^admin/metrics/', include('app_metrics.urls')),
 ]
